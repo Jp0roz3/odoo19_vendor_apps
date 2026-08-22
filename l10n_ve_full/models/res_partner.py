9583262facb34c488da62ad063e3ebda8458f6b8
Jean Perozo <jeanlisperozo@gmail.com>
2026-08-22 18:46:53 -0400

    feat(partner): optimizar posicionamiento de Municipio y Parroquia en bloque de direccion y sincronizar con estado nativo

--- l10n_ve_full/models/res_partner.py
+++ l10n_ve_full/models/res_partner.py
@@ -79,14 +79,39 @@ class ResPartner(models.Model):
     )
     l10n_ve_municipality_id = fields.Many2one(
         comodel_name='l10n_ve.municipality',
-        string='Municipio (Venezuela)',
+        string='Municipio',
         domain="[('state_id', '=', l10n_ve_state_id)]",
     )
     l10n_ve_parish_id = fields.Many2one(
         comodel_name='l10n_ve.parish',
-        string='Parroquia (Venezuela)',
+        string='Parroquia',
         domain="[('municipality_id', '=', l10n_ve_municipality_id)]",
     )
+
+    @api.onchange('state_id')
+    def _onchange_native_state_id(self):
+        """Sincroniza el estado nativo de Odoo con el catálogo de estados de Venezuela."""
+        if self.state_id:
+            ve_state = self.env['l10n_ve.state'].search([
+                '|', ('name', 'ilike', self.state_id.name),
+                ('code', 'ilike', self.state_id.code or '')
+            ], limit=1)
+            if ve_state:
+                self.l10n_ve_state_id = ve_state
+        else:
+            self.l10n_ve_state_id = False
+
+    @api.onchange('l10n_ve_municipality_id')
+    def _onchange_l10n_ve_municipality_id(self):
+        """Al seleccionar un municipio, autocompleta el estado correspondiente."""
+        if self.l10n_ve_municipality_id and self.l10n_ve_municipality_id.state_id:
+            self.l10n_ve_state_id = self.l10n_ve_municipality_id.state_id
+            if not self.state_id:
+                st = self.env['res.country.state'].search([
+                    ('name', 'ilike', self.l10n_ve_municipality_id.state_id.name)
+                ], limit=1)
+                if st:
+                    self.state_id = st
 
     # ------------------------------------------------------------------
     # Régimen fiscal de ISLR
